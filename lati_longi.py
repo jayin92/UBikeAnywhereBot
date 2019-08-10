824f1aefe302de881bcde5a28150a2b56384dd6a
hetty <ibuwang@gmail.com>
2019-08-11 01:08:54 +0800

    add feature
    1. 回傳經緯度
    2. 關鍵字

---
+++ lati_longi.py
@@ -0,0 +1,12 @@
+#利用經緯度回傳目的地的地圖
+import api
+from api import lat #緯度
+from api import lon #經度
+
+def locate (lat, lon):
+    sendLocation(
+        chat_id = ' ' #users
+        latitude = lat #緯度
+        longitude = lon #經度
+    )
+
---
+++ mainwords.py
@@ -0,0 +1,10 @@
+#關鍵字給API
+import bike.py
+from bike import 'words'
+
+search_words = list()
+
+def search ('words'):
+    for i in range (0, len(words), 1):
+        search_words[i] = words[i]
+
